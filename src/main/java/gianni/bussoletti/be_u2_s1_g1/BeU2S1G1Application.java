fc6f8e68d950995a6140fdd87eab22dde88f2b70
giannibussoletti <bussolettigianni@gmail.com>
2026-06-30 22:42:39 +0200

    recovered notes

--- src/main/java/gianni/bussoletti/be_u2_s1_g1/BeU2S1G1Application.java
+++ src/main/java/gianni/bussoletti/be_u2_s1_g1/BeU2S1G1Application.java
@@ -8,10 +8,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
+// La classe main viene automaticamente con SpringBootApplication
+// è predisposta per fare alcune cose sottobanco
+// SpringBoot gestisce tutte le sue dipendenze grazie a due principali dipendenze principali
+// spring-boot-starter e spring-boot-starter-test che gestiscono tutto.
 public class BeU2S1G1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(BeU2S1G1Application.class, args);
+        // Questo è un metodo già preimpostato
+        // Questo run è quello che permette a spring di funzionare
+        // entrambe non abbiamo bisogno di modificare
+        // Il codice va messo sotto il run
+        // Ma in una applicazione finita, la logica verrà gestita altrove.
+        // E non sul main
+
 
         //--------------METODO CREAZIONE OGGETTI CON I BEANS----------------
         // 1. Devo accedere all'ApplicationContext creando un oggetto di tipo AnnotationConfigApplicationContext
@@ -31,3 +42,4 @@ public class BeU2S1G1Application {
 
 
 }
+
